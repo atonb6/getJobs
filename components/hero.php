951c6a0bfc526261fdc6c42f40fe9c44a410234e
Philippe Paris <contacto@philippeparis.net>
2020-09-01 20:28:35 -0400

    image update

--- components/hero.php
+++ components/hero.php
@@ -2,43 +2,43 @@
 	<div class="hero__slider">
 
 		<picture class="hero__slider__img">
-            <source media="(max-width: 549px)" srcset="./dist/img/heroa.jpg">
-            <source media="(min-width: 550px)" srcset="./dist/img/heroa.jpg">
-            <img class="hero__slider__img__img-base" src="./dist/img/heroa.jpg" alt="Hero">
+			<source media="(max-width: 549px)" srcset="./dist/img/fbg.jpg">
+			<source media="(min-width: 550px)" srcset="./dist/img/fbg.jpg">
+			<img class="hero__slider__img__img-base" src="./dist/img/fbg.jpg" alt="Hero">
+		</picture>
+
+<!-- 		<picture class="hero__slider__img">
+			<source media="(max-width: 549px)" srcset="./dist/img/herob.jpg">
+			<source media="(min-width: 550px)" srcset="./dist/img/herob.jpg">
+			<img class="hero__slider__img__img-base" src="./dist/img/herob.jpg" alt="Hero">
 		</picture>
 
 		<picture class="hero__slider__img">
-            <source media="(max-width: 549px)" srcset="./dist/img/herob.jpg">
-            <source media="(min-width: 550px)" srcset="./dist/img/herob.jpg">
-            <img class="hero__slider__img__img-base" src="./dist/img/herob.jpg" alt="Hero">
+			<source media="(max-width: 549px)" srcset="./dist/img/heroc.jpg">
+			<source media="(min-width: 550px)" srcset="./dist/img/heroc.jpg">
+			<img class="hero__slider__img__img-base" src="./dist/img/heroc.jpg" alt="Hero">
 		</picture>
 
 		<picture class="hero__slider__img">
-            <source media="(max-width: 549px)" srcset="./dist/img/heroc.jpg">
-            <source media="(min-width: 550px)" srcset="./dist/img/heroc.jpg">
-            <img class="hero__slider__img__img-base" src="./dist/img/heroc.jpg" alt="Hero">
+			<source media="(max-width: 549px)" srcset="./dist/img/herod.jpg">
+			<source media="(min-width: 550px)" srcset="./dist/img/herod.jpg">
+			<img class="hero__slider__img__img-base" src="./dist/img/herod.jpg" alt="Hero">
 		</picture>
 
 		<picture class="hero__slider__img">
-            <source media="(max-width: 549px)" srcset="./dist/img/herod.jpg">
-            <source media="(min-width: 550px)" srcset="./dist/img/herod.jpg">
-            <img class="hero__slider__img__img-base" src="./dist/img/herod.jpg" alt="Hero">
-		</picture>
+			<source media="(max-width: 549px)" srcset="./dist/img/heroe.jpg">
+			<source media="(min-width: 550px)" srcset="./dist/img/heroe.jpg">
+			<img class="hero__slider__img__img-base" src="./dist/img/heroe.jpg" alt="Hero">
+		</picture> -->
 
-		<picture class="hero__slider__img">
-            <source media="(max-width: 549px)" srcset="./dist/img/heroe.jpg">
-            <source media="(min-width: 550px)" srcset="./dist/img/heroe.jpg">
-            <img class="hero__slider__img__img-base" src="./dist/img/heroe.jpg" alt="Hero">
-		</picture>
-		
 	</div>
 	<div class="container">
 		<div class="row justify-content-center text-center">
 			<div class="col-md-8 absolute center-absolute hero__forms">
-			<form action="resultados.php">
-				<img class="hero__logo" src="./dist/img/logo.svg" alt="Logo">
-				<h1 class="hero__title">Tu Plataforma Colaborativa e Inclusiva de Búsqueda de trabajo</h1>
-				
+				<form action="resultados.php">
+					<img class="hero__logo" src="./dist/img/logo.svg" alt="Logo">
+					<h1 class="hero__title">Tu Plataforma Colaborativa e Inclusiva de Búsqueda de trabajo</h1>
+
 					<fieldset class="text-center mt-5">
 						<input class="input input--select" type="text" placeholder="Puesto, empresa o palabra clave">
 						<select class="select select--input" name="search">
