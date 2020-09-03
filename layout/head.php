6ffdd4d7d4753557f8325fc3b1f9f0ea630c5a96
Philippe Paris <contacto@philippeparis.net>
2020-09-03 17:36:08 -0400

    favicon

--- layout/head.php
+++ layout/head.php
@@ -7,6 +7,25 @@
   <meta name="description" content="GetJobs">
   <meta name="author" content="Consciencia Digital">
 
+  <link rel="apple-touch-icon" sizes="57x57" href="dist/img/favicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="dist/img/favicon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="dist/img/favicon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="dist/img/favicon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="dist/img/favicon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="dist/img/favicon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="dist/img/favicon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="dist/img/favicon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="dist/img/favicon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="dist/img/favicon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="dist/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="dist/img/favicon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="dist/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="dist/img/favicon/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="dist/img/favicon/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+
   <title>GetJobs</title>
 
   <!-- Theme CSS - Incluye todo -->
@@ -16,14 +35,14 @@
 
 <body>
 
-	
+
   <?php if (basename($_SERVER['SCRIPT_NAME']) == 'index.php') : ?>
 
     <header class="header absolute">
-    <nav id="details-nav" class="navbar navbar-expand-lg navbar-light">
+      <nav id="details-nav" class="navbar navbar-expand-lg navbar-light">
 
-  <?php else : ?>
-    <header class="header">
-    <nav id="details-nav" class="navbar navbar-expand-lg navbar-blue">
+      <?php else : ?>
+        <header class="header">
+          <nav id="details-nav" class="navbar navbar-expand-lg navbar-blue">
 
-  <?php endif ?>
+          <?php endif ?>
