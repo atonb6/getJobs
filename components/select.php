4a526e3269598b8de459babeb88e7cb972402132
Philippe Paris <contacto@philippeparis.net>
2020-09-07 13:04:01 -0300

    selector chevron update

--- components/select.php
+++ components/select.php
@@ -2,12 +2,12 @@
     <div class="container">
 
 
-        <form action="results.php">
+        <form action="results-filter.php">
             <div class="row justify-content-center">
                 <div class="col-md-12">
                     <div class="row">
                         <div class="col px-0 select__icon">
-                            <select class="select select--input" name="industria">
+                            <select class="select select--input-filter" name="industria">
                                 <option value="value1">industria</option>
                                 <option value="value2">Alimentos</option>
                                 <option value="value2">Alimentos</option>
@@ -19,7 +19,7 @@
                         </div>
 
                         <div class="col px-0 select__icon">
-                            <select class="select select--input" name="area">
+                            <select class="select select--input-filter" name="area">
                                 <option value="value1">Gerencia General</option>
                                 <option value="value1">Gerencia General</option>
                                 <option value="value1">Gerencia General</option>
@@ -29,7 +29,7 @@
 							</span>
                         </div>
                         <div class="col px-0 select__icon">
-                            <select class="select select--input" name="nivelcargo">
+                            <select class="select select--input-filter" name="nivelcargo">
                                 <option value="value1">C-Level / Directivos</option>
                                 <option value="value2">C-Level / Directivos</option>
                             </select>
@@ -38,7 +38,7 @@
 							</span>
                         </div>
                         <div class="col px-0 select__icon">
-                            <select class="select select--input" name="region">
+                            <select class="select select--input-filter" name="region">
                                 <option value="value1">Región</option>
                                 <option value="value2">Región de Arica y Parinacota</option>
                                 <option value="value2">Región de Arica y Parinacota</option>
