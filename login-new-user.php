56e94a90ff1e6bf47ef16a864390a5de08ebe8ad
Philippe Paris <contacto@philippeparis.net>
2020-09-07 10:01:00 -0300

    update login

---
+++ login-new-user.php
@@ -0,0 +1,5 @@
+<?php include('layout/head.php'); ?>
+<?php include('layout/header.php'); ?>
+<?php include('components/results-single-title.php'); ?>
+<?php include('components/login-new-user.php'); ?>
+<?php include('layout/footer.php'); ?>
