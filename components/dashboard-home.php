b0e55530ae782efa3db12c781f18755248c4043f
Philippe Paris <contacto@philippeparis.net>
2020-10-15 00:19:49 -0300

    lightbox width tabs

--- components/dashboard-home.php
+++ components/dashboard-home.php
@@ -24,13 +24,13 @@
             <div class="col-lg-8 col-sm-12">
                 <div class="row align-items-center">
                     <div class="col-12 col-lg-4">
-                        <a class="btn btn--blue w-100 sameheight" href="#">Agregar a mis contactos</a>
+                        <a class="btn btn--blue w-100 sameheight" href="#" data-toggle="modal" data-target="#modaladd">Agregar a mis contactos</a>
                     </div>
                     <div class="col-12 col-lg-4">
-                        <a class="btn btn--border-blue w-100 sameheight" href="#">Ver Contactos</a>
+                        <a class="btn btn--border-blue w-100 sameheight" href="#" data-toggle="modal" data-target="#modal">Ver Contactos</a>
                     </div>
                     <div class="col-12 col-lg-4">
-                        <a class="btn btn--border-blue w-100 sameheight" href="#">Enviar Mensaje</a>
+                        <a class="btn btn--border-blue w-100 sameheight" href="dash-user-inbox-message.php">Enviar Mensaje</a>
                     </div>
                 </div>
             </div>
@@ -49,7 +49,70 @@
                 <h2 class="profile-home__work">Experiencia</h2>
                 <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat autem quod dolorem tenetur adipisci corporis natus sit aspernatur! Veniam culpa consequatur, minus similique corrupti molestiae sunt magni laborum labore aliquam.
                 </p>
+            </div>
 
+
+            <div class="gj-modal modal fade text-center" id="modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+
+                        <div class="mt-2">
+                            <h5 class="white">Ver Contactos</h5>
+                        </div>
+
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="contactos-tab" data-toggle="tab" href="#contactos" role="tab" aria-controls="contactos" aria-selected="true">Contactos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="comun-tab" data-toggle="tab" href="#comun" role="tab" aria-controls="comun" aria-selected="false">En común</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="contactos" role="tabpanel" aria-labelledby="contactos-tab">
+                                <input class="search__input mt-3 mr-3 w-100" type="text" placeholder="Buscar contactos...">
+
+                                <div class="modal-body">
+                                    <div class="row">
+                                        <?php for ($i = 0; $i <= 10; $i++) : ?>
+                                            <div class="col-md-6">
+                                                <a href="#"><?php include('components/people-cards-simple.php') ?></a>
+                                            </div>
+                                        <?php endfor; ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="comun" role="tabpanel" aria-labelledby="comun-tab">
+                                <input class="search__input mt-3 mr-3 w-100" type="text" placeholder="Buscar contactos...">
+
+                                <div class="modal-body">
+                                    <div class="row">
+                                        <?php for ($i = 0; $i <= 5; $i++) : ?>
+                                            <div class="col-md-6">
+                                                <a href="#"><?php include('components/people-cards-simple.php') ?></a>
+                                            </div>
+                                        <?php endfor; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="gj-modal modal fade" id="modaladd" tabindex="-1" role="dialog" aria-labelledby="modaladdLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">¿Deseas agrear a tus contactos?</h5>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn--blue" data-dismiss="modal">Cancelar</button>
+                            <button type="button" class="btn btn--blue">Sí</button>
+                        </div>
+                    </div>
+                </div>
             </div>
     </section>
 
---
+++ components/people-cards-simple.php
@@ -0,0 +1,11 @@
+<a href="dash-user-home.php" class="mt-4 mb-4 people-cards d-flex align-items-center">
+    <div class="d-flex align-items-center">
+        <img class="people-cards__img" src="https://placeimg.com/80/80" alt="persona">
+        <div class="ml-3">
+            <h4><strong>Empresa en la que trabaja</strong></h4>
+            <h3 class="mt-2 mb-2"><strong>Nombre</strong></h3>
+            <h3 class="mt-2 mb-2">Título de cargo</h3>
+            <h4>Ciudad - Modalidad</h4>
+        </div>
+    </div>
+</a>
